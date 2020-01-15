55d17e4887aa187a9a6e21ba8cae3256abc15ce3
IDilantha <isurudilantha@gmail.com>
2020-01-16 02:13:37 +0530

    Add Datasource,Properties in HibernateConfig

--- src/main/java/io/github/idilantha/pos/HibernateConfig.java
+++ src/main/java/io/github/idilantha/pos/HibernateConfig.java
@@ -1,4 +1,32 @@
 package io.github.idilantha.pos;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+
+@Configuration
+@PropertySource("file:${user.dir}/resources/application.properties")
 public class HibernateConfig {
+
+    public DataSource dataSource(){
+        DriverManagerDataSource dmds = new DriverManagerDataSource();
+        dmds.setDriverClassName();
+        dmds.setUsername();
+        dmds.setPassword();
+        dmds.setUrl();
+        return dmds;
+    }
+
+    private Properties hibernateproperties(){
+        Properties properties = new Properties();
+        properties.put();
+        properties.put();
+        properties.put();
+        properties.put();
+        return properties;
+    }
 }
