caf10edce4f5471df8323ba0f70339e52835d6fc
IDilantha <isurudilantha@gmail.com>
2020-01-16 02:20:01 +0530

    Add LocalSessionFactory in HibernateConfig

--- src/main/java/io/github/idilantha/pos/HibernateConfig.java
+++ src/main/java/io/github/idilantha/pos/HibernateConfig.java
@@ -1,8 +1,12 @@
 package io.github.idilantha.pos;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 import javax.sql.DataSource;
 import java.util.Properties;
@@ -12,16 +16,29 @@ import java.util.Properties;
 @PropertySource("file:${user.dir}/resources/application.properties")
 public class HibernateConfig {
 
-    public DataSource dataSource(){
+    @Autowired
+    Environment env;
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
+        LocalSessionFactoryBean lsbf = new LocalSessionFactoryBean();
+        lsbf.setDataSource(dataSource);
+        lsbf.setPackagesToScan("io.github.idilantha.pos.entity");
+        lsbf.setHibernateProperties(hibernateproperties());
+        return lsbf;
+    }
+
+    @Bean
+    public DataSource dataSource() {
         DriverManagerDataSource dmds = new DriverManagerDataSource();
-        dmds.setDriverClassName();
+        dmds.setDriverClassName(env.getRequiredProperty("hibernate.connection.driver_class"));
         dmds.setUsername();
         dmds.setPassword();
         dmds.setUrl();
         return dmds;
     }
 
-    private Properties hibernateproperties(){
+    private Properties hibernateproperties() {
         Properties properties = new Properties();
         properties.put();
         properties.put();
